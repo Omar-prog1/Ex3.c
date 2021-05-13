1dd146e8d2e0ae58da45475644d7e25b384e8021
Omar-prog1 <82549988+Omar-prog1@users.noreply.github.com>
2021-05-12 21:54:44 -0500

    Add files via upload

---
+++ Ex3.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ int main ()
+{
+    float Fresa, Cincokilos, $13, precio;
+    printf("\n\n Hernandez Flores Ricardo Omar");
+    printf ( "\n\n comprando fruta");
+    printf ( "\n\n precio de cualquier fruta $13");
+    printf ( "\n\n ingrese la fruta que selecciono");
+    scanf ( "%f", &Fresa);
+    printf ( "\n\n ingrese el peso de la futa seleccionada");
+    scanf ( "%f", &CincoKilos);
+    precio= Cincokilos * $13;
+    printf ( "\n\n el presio total es %f", precio);
+    return 0;
+
+}
